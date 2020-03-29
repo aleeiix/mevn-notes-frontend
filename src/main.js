eaf03c407f19080ddc8f5d016053734d32bb013d
Aleix Riba <aleix.riba@cabsa.es>
2020-03-29 14:14:07 +0200

    install dependency and configure

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+axios.defaults.baseURL = "http://localhost:3000/api";
+
+Vue.use(VueAxios, axios);
+
 Vue.config.productionTip = false;
 
 new Vue({
